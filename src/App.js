ad2ec716cf7bbd6b1274b2c01488266f2b2fe8df
Jay <jaylingelbach@users.noreply.github.com>
2022-03-16 16:13:00 -0500

    basic footer added and some header links moved to footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import ResponsiveAppBar from './components/header';
+import SimpleBottomNavigation from './components/footer';
 
 function App() {
   return (
     <div className="App">
       <ResponsiveAppBar />
       <header className="App-header">
-        
-        <a href='/' id='startTag'> START HERE</a>
+        <a href='/' id='startTag'> Build it</a>
       </header>
+      <SimpleBottomNavigation />
     </div>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,18 +1,21 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
 import IconButton from '@mui/material/IconButton';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import Logo from '../assets/bbcLogo.png';
 
-const pages = ['About', 'How it works', 'Pedals', 'Contact'];
+
+// Maybe move about to footer.  Footer could include about, location, 
+// social media etc
+const pages = ['How it works', 'FAQ', 'Contact'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const ResponsiveAppBar = () => {
@@ -51,15 +54,6 @@ const ResponsiveAppBar = () => {
               component="div"
               sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
             >
-              <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
             <Box
               component="img"
               sx={{
