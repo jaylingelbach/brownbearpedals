ed5fd6e416211dcf74f2f15a4a806f2fef738f6c
Jay <jaylingelbach@users.noreply.github.com>
2022-03-22 13:03:27 -0500

    FAQ updated

---
+++ src/faqQuestions/faqQuestions.js
@@ -0,0 +1,47 @@
+const faqQuestions = [
+{
+    summary: 'There might be a problem with my pedal. What do I do?',
+    details: `Please contact us here. Once the troubleshooting procedure has 
+    been completed (over 75% of issues are fixed this way). If the pedal needs 
+    to come back to us, you will need to complete the return 
+    form (with the RA number provided by us) and include it in the 
+    package. Details of where to send it to you will be provided with
+    the RA number.`
+},
+{
+    summary:'What is the warranty on your pedals?',
+    details: `1 year warranty includes parts and labor and return shipping back 
+    to you within Continental USA. You are responsible to ship it to 
+    us. If we open it up and find repairs are needed because of 
+    misuse, abuse (accidental or not) or neglect than we cannot 
+    cover the repairs for free.`
+},
+{
+    summary: '****What is the return policy? FIGURE OUT****',
+    details: 'FIGURE OUT BEFORE RELEASE'
+},
+{
+    summary: 'Do your pedals ship with power supply units? (PSU) ?',
+    details: `Our pedals do not ship with power supply units. Importantly
+    they also do not have the ability to run off a 9v battery either. You will 
+    need to power these with a suitable (preferably isolated) 9v PSU that is 
+    intended for guitar pedal power only.`
+},
+{
+    summary: 'My pedal started having a noise problem',
+    details: `Has anything else in the signal chain changed? Many noise problems
+    are due to an inferior power supply. Have you tried it by itself? 
+    When troubleshooting it’s best to isolate the assumed source. 
+    Have you tried it with different cables? 
+    Are you using the gain channel of your amp or clean channel? 
+    Most higher gain pedals sound best into the clean channel of an amp.`
+},
+{
+    summary: 'What are the power requirements for your pedals?',
+    details: `We advise you use a good quality power supply for your pedals. Each pedal was designed to run at 9v DC and
+     will perform as designed when being run that way. Also, ensure that your 
+    power supply is a negative center pin.`
+},
+]
+
+export default faqQuestions;
